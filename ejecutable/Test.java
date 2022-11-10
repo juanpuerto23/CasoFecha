ae9e44546fdb583d73533e22957fed50f17b2352
juanpuerto23 <juan2224511@correo.uis.edu.co>
2022-11-10 07:56:10 -0500

    Agregando nuevos casos

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -4,15 +4,31 @@ import modelo.Fecha;
 
 public class Test {
     public static void main(String[] args) {
-        Fecha f = new Fecha();
-        f.setDia(2);
-        f.setMes(10);
-        f.setAnio(1970);
-
+        Fecha f = new Fecha(2, 10, 1970);
         System.out.println("Dia: " + f.getDia());
         System.out.println("Mes: " + f.getMes());
         System.out.println("Año: " + f.getAnio());
         System.out.println(f);
+
+        Fecha f2 = new Fecha();
+        System.out.println("Clase Fecha vacia: " + f2);
+
+        Fecha f3 = new Fecha ("10/11/2022");
+        System.out.println("Clase Fecha Tipo String: " + f3);
+
+        Fecha otra = new Fecha(5, 5, 1995);
+        otra.setDia(5);
+        otra.setMes(5);
+        otra.setAnio(1995);
+
+        System.out.println(otra.equals(f));
+
+        if (otra.equals(f) == false){
+            System.out.println("Las dos fechas son diferentes");
+        }
+
+        if (otra.equals(f) == true) {
+            System.out.println("Las dos fechas son iguales");
+        }
     }
-    
 }
--- modelo/Fecha.java
+++ modelo/Fecha.java
@@ -7,6 +7,48 @@ public class Fecha {
     private int mes;
     private int anio;
 
+    // Metodos
+    // Constructor con 3 parametros
+    /*Esto es para que en vez de usar Set para crear la fecha despues de creada la clase se crea la clase con
+      esos parametros */
+    public Fecha(int d, int m, int a)
+    {
+        this.dia = d;
+        this.mes = m;
+        this.anio = a;
+    }
+
+    // Constructor sin parametros
+    public Fecha()
+    {
+        this.dia = 0;
+        this.mes = 0;
+        this.anio = 0;
+    }
+
+    // Constructor con parametro String dd/mm/aa
+    public Fecha(String s)
+    {
+        // Buscamos la primera ocurrencia de '/'
+        int pos1 = s.indexOf('/');
+        
+        // Buscamos la ultima ocurrencia de '/'
+        int pos2 = s.lastIndexOf('/');
+
+        // Extraemos el día
+        String sDia = s.substring(0, pos1);
+        dia = Integer.parseInt(sDia);
+
+        // Extraemos el mes
+        String sMes = s.substring(pos1 + 1, pos2);
+        mes = Integer.parseInt(sMes);
+
+        // Extraemos el año
+        String sAnio = s.substring(pos2 +1);
+        anio = Integer.parseInt(sAnio);
+    }
+
+
     // Metodos (de acceso(setter y getter))
     public void setAnio(int anio) {
         this.anio = anio;
@@ -29,5 +71,9 @@ public class Fecha {
     public String toString()
     {
         return dia + "/" + mes + "/" + anio;
+    }
+    public boolean equals(Object o){
+        Fecha otra = (Fecha)o;
+        return (dia==otra.dia) && (mes == otra.mes) && (anio == otra.anio);
     }
 }
